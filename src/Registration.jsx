491f395619bfde8c18cc4e61da4fb39966b52375
srivensy <srivensyshortterm@gmail.com>
2025-08-05 20:11:31 +0530

    Resolved issues in Registration page

--- src/Registration.jsx
+++ src/Registration.jsx
@@ -17,7 +17,10 @@ const Registration = () => {
     const {firstname,lastname,dob,phone,email,password} = data
 
     const inputhandeler = (event) =>{
-        setData({...data,[event.target.name]:[event.target.value]})
+        setData({
+            ...data,
+            [event.target.name]:event.target.value
+        })
     }
 
     const submithandler = (event) =>{
@@ -31,61 +34,30 @@ const Registration = () => {
             <form onSubmit={submithandler}>
                 <div>
                     <label>First Name</label>
-                    <input type="text" name={firstname} onChange={inputhandeler} class="form-control"/>
+                    <input type="text" name="firstname" value={firstname} onChange={inputhandeler} className="form-control"/>
                 </div>
                 <div>
                     <label >Last Name</label>
-                    <input type="text" name={lastname} onChange={inputhandeler} class="form-control" id="exampleInputPassword1"/>
+                    <input type="text" name='lastname' value={lastname} onChange={inputhandeler} className="form-control" />
                 </div>
                 <div>
                     <label>Date of Birth</label>
-                    <input type="date" name={dob} onChange={inputhandeler} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <input type="date" name='dob' value={dob} onChange={inputhandeler} className="form-control" />
                 </div>
                 <div>
                     <label >Phone Number</label>
-                    <input type="number" name={phone} onChange={inputhandeler} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <input type="number" name='phone' value={phone} onChange={inputhandeler} className="form-control"/>
                 </div>
                 <div>
                     <label >Email address</label>
-                    <input type="email" name={email} onChange={inputhandeler}  class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <input type="email" name='email' value={email} onChange={inputhandeler}  className="form-control" />
                 </div>
-                <div class="mb-1">
+                <div className="mb-1">
                     <label >Password</label>
-                    <input type="password" name={password} onChange={inputhandeler} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <input type="password" name='password' value={password} onChange={inputhandeler} className="form-control" />
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
-
-            {/* <h1>Registration Form</h1><b/>
-            <form>
-                <div className='mb-1'>
-                <label>First Name</label><br/>
-                <input type='text' placeholder='First Name'/>
-                </div >
-                <div className='mb-1'>
-                <label>Last Name</label><br/>
-                <input type='text' placeholder='Last Name'/>
-                </div >
-                <div  className='mb-1'>
-                    <label>Date of Birth</label><br/>
-                    <input type='date' placeholder='Enter yout DOB'/>
-                </div>
-                <div  className='mb-1'>
-                    <label>Phone Number</label><br/>
-                    <input type='number' placeholder='Enter your phone number'/>
-                </div>
-                <div  className='mb-1'>
-                    <label>Email</label><br/>
-                    <input type='email' placeholder='Enter your email'/>
-                </div>
-                <div  className='mb-1'>
-                    <label>Password</label><br/>
-                    <input type='password' placeholder='Enter your password'/>
-                </div>
-                <div  className='mb-1'>
-                    <button>Submit</button>
-                </div>
-            </form> */}
         </div>
     </div>
   )
